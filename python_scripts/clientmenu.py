23c60d90754ccc1330cf21ad65c7507f6c1cd115
semihM <smhmmc98@gmail.com>
2021-01-31 22:54:09 +0300

    Add some of the python scripts used in development

---
+++ python_scripts/clientmenu.py
@@ -0,0 +1,169 @@
+
+SOUND_DEFAULT = "buttons/button14"
+
+DIRECTION_BACK = " << Back"
+DIRECTION_BACK_INDEX = 8
+DIRECTION_NEXT = " >> Next"
+DIRECTION_EXIT = " Exit"
+
+class Templates:
+    def cmddefault(menuname):
+        return "play "+SOUND_DEFAULT+";"+"scripted_user_func ;show_menu "+menuname
+
+    def exitdefault():
+        return "play "+SOUND_DEFAULT+";"
+
+    def labeldefault(index):
+        return " Label_"+str(index)
+
+    def cmdpropdynamicdefault(menuname):
+        return "play "+SOUND_DEFAULT+";"+"scripted_user_func prop,dynamic,;show_menu "+menuname
+
+    def labelpropdynamicdefault(index):
+        return " dynamic_prop_"+str(index)
+
+class element:
+    def __init__(self,index=1,command="",label="",_next=None,**kwargs):
+        self.index = index
+        self.command = command
+        self.label = label
+        self.extras = kwargs
+        self.next = _next
+
+    def copy(self):
+        return element(self.index,self.command,self.label)
+
+    def connect(self,other):
+        self.next = other
+        split = self.command.split(";")
+        self.command = ";".join(split[:-1]) + "show_menu " + other.name
+
+    def printstr(self,prefix=""):
+        s = prefix + '"'+str(self.index)+'"\n'+prefix+'{\n\t'
+        s += prefix + '"command"\t"'+self.command+'"\n\t'
+        s += prefix + '"label"\t\t"'+str(self.label)+'"\n'+prefix+'}'
+        return s
+
+class direlement(element):
+    def __init__(self,index=DIRECTION_BACK_INDEX,other=None,direction=DIRECTION_BACK,extracommand="",sound=SOUND_DEFAULT,**kwargs):
+        if other is None:
+            super().__init__(index,"play "+sound+";"+extracommand+";show_menu ",direction,None,**kwargs)
+        else:
+            super().__init__(index,"play "+sound+";"+extracommand+";show_menu "+other.name,direction,other,**kwargs)
+
+class subdirelement(element):
+    def __init__(self,index=1,other=None,label="",extracommand="",sound=SOUND_DEFAULT,**kwargs):
+        if other is None:
+            super().__init__(index,"play "+sound+";"+extracommand+";"+"show_menu ",label,None,**kwargs)
+        else:
+            super().__init__(index,"play "+sound+";"+extracommand+";"+"show_menu "+other.name,label,other,**kwargs)
+
+class menu:
+    def __init__(self,prefix="",name="",suffix="",title="",elements=[],_prev=None,_next=None):
+        self.prefix = str(prefix)
+        self.suffix = str(suffix)
+        self.name = self.prefix + str(name) + self.suffix
+        self.title = str(title)
+        self.elements = []
+        self.arraycopy(elements,self.elements)
+        self.prev = _prev
+        self.next = _next
+    
+    def arraycopy(self,arr,newarr):
+        for e in arr:
+            newarr.append(e.copy())
+        return newarr
+
+    def printallprevstr(self):
+        s = ""
+        current = self.prev
+        while current is not None:
+            s += current.printstr() + "\n"
+            current = current.prev
+        return s
+
+    def printstr(self):
+        length = len(self.elements)
+        s = '"'+self.name+'"\n{\n\t'
+        s += '"Title"\t\t"'+self.title+'"\n'
+        for i in range(length):
+            element = self.elements[i]
+            s += element.printstr("\t")
+            if i != length - 1:
+                s += "\n"
+        s += "\n}"
+        return s
+
+    def printallnextstr(self):
+        s = ""
+        current = self.next
+        while current is not None:
+            s += current.printstr() + "\n"
+            current = current.next
+        return s
+
+    def printallstr(self):
+        return self.printallprevstr() + self.printstr() + self.printallnextstr()
+
+def connectMenusForward(submenus,index):
+    for i in range(len(submenus)-1):
+        submenus[i].elements[index].connect(submenus[i+1])
+        submenus[i].next = submenus[i].elements[index].next
+
+def createDirElement(_prev,_next,index=DIRECTION_BACK_INDEX):
+    return [direlement(index,_prev),direlement(index+1,_next,DIRECTION_NEXT)]
+
+def createExitElement(index=0):
+    return element(index,Templates.exitdefault(),DIRECTION_EXIT)
+
+def createDeleteElement(name,index=7):
+    return element(index,Templates.deletefault(name))
+
+def addFirstPageExtras(lis,_prev):
+    lis.extend(createDirElement(_prev,None))
+    lis.append(createExitElement())
+
+def createEmptyMenu(name,title,pageCount=1,elementEachPage=6,_prev=None,functemplate=None,labeltemplate=None):
+    submenus = []
+    elements = []
+    elementEachPage = min(max(1,elementEachPage),7)
+    pageCount = max(1,pageCount)
+
+    if functemplate is None:
+        functemplate = Templates.cmddefault
+
+    if labeltemplate is None:
+        labeltemplate = Templates.labeldefault
+
+    i = 1
+    for index in range(1,elementEachPage+1):
+        elements.append(element(index,functemplate(name),labeltemplate(index)))
+    addFirstPageExtras(elements,_prev)
+    submenu = menu(name=name,suffix=i,title=title+" "+str(i),elements=elements)
+    submenus.append(submenu)
+    i += 1
+
+    while i <= pageCount:
+        elements = []  
+        for index in range(1,elementEachPage+1):
+            elements.append(element(index,functemplate(name+str(i)),labeltemplate(index)))
+        if i == pageCount:
+            elements.append(direlement(DIRECTION_BACK_INDEX,submenus[i-2]))
+        else:
+            elements.extend(createDirElement(submenus[i-2],None))
+        elements.append(createExitElement())
+        submenu = menu(name=name,suffix=i,title=title+" "+str(i),elements=elements)
+        submenus.append(submenu)
+        i += 1
+
+    connectMenusForward(submenus,elementEachPage+1)
+    return submenus
+
+basename = "prop_dynamic"
+basetitle = "Dynamic props"
+propPerPage = 6
+pageCount = 3
+
+propdynamicmenuempty = createEmptyMenu(basename,basetitle,propPerPage,pageCount,None,Templates.cmdpropdynamicdefault,Templates.labelpropdynamicdefault)
+
+print(propdynamicmenuempty[0].printallstr())
---
+++ python_scripts/getnetprops.py
@@ -0,0 +1,252 @@
+from bs4 import BeautifulSoup
+from urllib import request
+
+class classtable:
+    def __init__(self,name="",subtables={}):
+        self.name = name
+        self.tables = subtables
+        self.depth = subtables.__len__()
+        self.tablecount = sum([lis.__len__() for lis in self.tables.values()])
+        self.membercount = sum([sum([subtbl.members.__len__() for subtbl in lis]) for lis in self.tables.values()])
+    
+    def gettable(self,tablename):
+        i = 1
+        length = self.tables.__len__()
+
+        while i < length:
+            for tbl in self.tables[i]:
+                if tbl.name == tablename:
+                    return tbl
+            
+            i += 1
+        
+        return None
+    
+    def getmember(self,membername,tablename=""):
+        if tablename == "":
+            i = 1
+            length = self.tables.__len__()
+
+            while i < length:
+                for tbl in self.tables[i]:
+                    member = tbl.getmember(membername)
+                    if member is not None:
+                        return member
+                
+                i += 1
+            
+            return None
+        else:
+            tbl = self.gettable(tablename)
+            if tbl is None:
+                return None
+            else:
+                return tbl.getmember(membername)
+    
+    def printbydepth(self,tblprefix="+",memberprefix="-"):
+        for depth in self.tables.values():
+            for tbl in depth:
+                print(tbl.printstr(tblprefix) + " (Depth:"+ str(tbl.depth) + ")")
+                for m in tbl.members:
+                    print(m.printstr(memberprefix))
+
+    def printbydepthfull(self,tblprefix="+",memberprefix="-"):
+        for depth in self.tables.values():
+            for tbl in depth:
+                print(tbl.printstrfull(tblprefix) + " (Depth:"+ str(tbl.depth) + ")")
+                for m in tbl.members:
+                    print(m.printstrfull(memberprefix))
+            
+class subtable:
+    def __init__(self,string,parent=None,members=[],clean=True,name="",depth=1):
+        if clean:
+            self.string = string.replace("(","").replace(")","").replace(":","").replace("Sub-Class Table ","")
+        else:
+            self.string = string
+
+        if parent is None:
+            self.parent = None
+        else:
+            self.parent = parent.copy()
+
+        self.depth = depth
+        self.name = name
+        self.members = self.arraycopy(members)
+
+        if clean:
+            self.clean()
+    
+    def arraycopy(self,arr):
+        n = []
+        for m in arr:
+            n.append(m.copy())
+        return n
+
+    def copy(self):
+        return subtable(self.string,self.parent,self.members,False,self.name,self.depth)
+
+    def clean(self):
+        split = self.string.split(" ")
+        self.depth = split.count("") + 1
+        self.name = split[-1]
+    
+    def getmember(self,name):
+        for np in self.members:
+            if np.name == name:
+                return np
+        return None
+
+    def printstr(self,prefix=""):
+        return (prefix*(self.depth-1)) + self.name
+
+    def printstrfull(self,prefix=""):
+        return (prefix*(self.depth-1)) + self.name + " " + str(len(self.members))
+
+    def printstrparented(self,prefix=""):
+        if self.parent is None:
+            return (prefix*(self.depth-1)) + self.name
+        else:
+            return self.parent.printstrparented(prefix) + "." + self.name
+
+class netprop:
+    def __init__(self,string,parent,clean=True,name="",depth=1,offset=0,typ="",bits=0):
+        if clean:
+            self.string = string.replace("(","").replace(")","").replace("-Member","").replace(":","").replace("-","")
+        else:
+            self.string = string
+
+        if parent is None:
+            self.parent = None
+        else:
+            self.parent = parent
+
+        self.depth = depth
+        self.name = name
+        self.offset = offset
+        self.type = typ
+        self.bits = bits
+        if clean:
+            self.clean()
+    
+    def clean(self):
+        split = self.string.split(" ")
+        namefound = False
+        i = 0
+        while i < len(split):
+            ch = split[i]
+            if ch == "":
+                self.depth += 1
+                i += 1
+                continue
+
+            if not namefound:
+                self.name = ch
+                namefound = True
+                i += 1
+            
+            if ch == "offset":
+                self.offset = int(split[i+1])
+                i += 2
+            
+            if ch == "type":
+                self.type = split[i+1]
+                i += 2
+            
+            if ch == "bits":
+                self.bits = int(split[i+1])
+                i += 1
+                break
+    
+    def copy(self):
+        return netprop(self.string,self.parent,False,self.name,self.depth,self.offset,self.type,self.bits)
+
+    def printstr(self,prefix=""):
+        return (prefix*(self.depth-1)) + self.name
+    
+    def printstrfull(self,prefix=""):
+        return (prefix*(self.depth-1)) + self.name + " " + self.type + " " + str(self.bits)
+
+    def printstrparented(self,prefix=""):
+        if self.parent is None:
+            return (prefix*(self.depth-1)) + self.name
+        else:
+            return self.parent.printstrparented(prefix) + "." + self.name
+
+def getPageContent(_url):
+    con = request.urlopen(request.Request(_url, headers={'User-Agent' : "Magic Browser"} ) )
+    return con.read()
+
+urls = [
+    "https://wiki.alliedmods.net/Left_4_Dead_2_Netprops",
+    "https://wiki.alliedmods.net/Left_4_Dead_2_Netprops_2",
+    "https://wiki.alliedmods.net/Left_4_Dead_2_Netprops_3"
+]
+
+contentelementtag = "div"
+contentelementclass = "mw-parser-output"
+
+netproptableheaderclass = "mw-headline"
+netproptableheadertag = "span"
+netproptabletag = "pre"
+
+subtablename = "Sub-Class Table"
+membername = "-Member:"
+netpropdict = dict()
+
+for url in urls:
+    soup = BeautifulSoup(getPageContent(url),features="html.parser").find(contentelementtag, {"class":contentelementclass})
+    
+    netproptablename = soup.find(netproptableheadertag,{"class":netproptableheaderclass})
+    netproptable = soup.find(netproptabletag)
+
+    while netproptable is not None and netproptablename is not None:
+        key = netproptablename.string.replace(":","")
+        vals = netproptable.string.split("\n")
+
+        currentparent = None
+        tables = {}
+        currentdepth = 1
+        for row in vals:
+            if row == "":
+                continue
+            
+            if subtablename in row:
+                currentparent = subtable(row,currentparent)
+                currentdepth = currentparent.depth
+                if currentdepth in tables.keys():
+                    tables[currentdepth].append(currentparent)
+                else:
+                    tables[currentdepth] = [currentparent]
+
+                #print(("-"*currentparent.depth)+currentparent.printstrparented())
+
+            elif membername in row:
+                netp = netprop(row,currentparent)
+
+                if netp.depth != currentdepth:
+                    currentdepth = netp.depth
+                    currentparent = tables[currentdepth-1][-1]
+                    netp.parent = currentparent
+
+                currentparent.members.append(netp)
+                
+                #print(("-"*netp.depth)+netp.printstrparented())
+
+        netpropdict[key] = classtable(key,tables)
+
+        netproptable = netproptable.find_next_sibling(netproptabletag)
+        netproptablename = netproptablename.find_next(netproptableheadertag)
+
+print(netpropdict.__len__())
+
+testclass = "Witch"
+testprop = "lengthprop6"
+subtableprefix = ">"
+memberprefix = " "
+
+print(netpropdict[testclass].depth)
+print(netpropdict[testclass].tablecount)
+print(netpropdict[testclass].membercount)
+print(netpropdict[testclass].getmember(testprop).printstrparented())
+
+netpropdict[testclass].printbydepthfull(subtableprefix,memberprefix)
